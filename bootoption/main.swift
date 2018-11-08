20819919c51a815988308f18edcb4e1acbee6fc7
vulgo <imvulgo@gmail.com>
2018-11-08 12:05:39 +0000

    Formula changes

--- bootoption/main.swift
+++ bootoption/main.swift
@@ -38,7 +38,7 @@ if Nvram.shared.emuVariableUefiPresent {
         Debug.fault("EmuVariableUefiPresent found in options")
 }
 
-var programInfo = ProgramInfo(name: "bootoption", version: "0.2.12", copyright: "Copyright © 2017-2018 vulgo", license: "This is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\nSee the GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>")
+var programInfo = ProgramInfo(name: "bootoption", version: "0.2.99", copyright: "Copyright © 2017-2018 vulgo", license: "This is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\nSee the GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>")
 
 Debug.log("Version %@", type: .info, argsList: programInfo.version)
 
